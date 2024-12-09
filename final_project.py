c49b36162fdc40a559afe1fe1c606a94ef496c44
julia-ashlyn <152316912+julia-ashlyn@users.noreply.github.com>
2024-12-09 12:27:54 -0500

    Add files via upload

---
+++ final_project.py
@@ -0,0 +1,14 @@
+### QUESTION 1 ###
+# Is there a statistically significant difference between the right eye and left eye in healthy individuals in terms of microvolts outputted?
+
+
+### QUESTION 2 ###
+# Is there a statistically significant difference between the voltage output via PERG signal between healthy individuals and individuals with retinitis pigmentosa?
+
+
+### QUESTION 3 ###
+# Is there a way to determine which eye in an individual is diseased?
+
+
+### QUESTION 4 ###
+# Is there a statisistically significant difference between the voltage output via PERG signal between healthy individuals with and without mercury poisoning?
